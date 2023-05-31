080837d83f50c94b0064dae964cdeb4eb20c5e6f
Yanzz <yonielkpo@gmail.com>
2023-05-31 16:15:14 -0300

    fixed completly

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -3,25 +3,24 @@ import Product from "@/backend/model/Product";
 import { NextResponse } from "next/server";
 
 export async function POST (req) {
+  const body = await req.json()
   try {
-    
     await dbConnect()
     
-    const product = new Product({
-    "name": "Beats Solo3 Wireless On-Ear Headphones",
-    "description": "The Beats Solo3 Wireless On-Ear Headphones are a premium set of headphones that feature Bluetooth connectivity, up to 40 hours of battery life, and on-ear controls for adjusting volume and switching tracks. They have a sleek, stylish design and come in a variety of colors. They also have a built-in microphone for taking calls and use the Apple W1 chip for easy and stable connectivity with Apple devices.",
-    "price": 199.95,
-    "seller": "Beats by Dr. Dre",
-    "stock": 500,
-    "category": "Headphones"
-  }
-  )
-  await product.save()
-  return NextResponse.json(product, {status: 200})
+    const product = new Product(body)
+    console.log({product})
+    await product.save()
+    return NextResponse.json(product, {status: 200})
   } catch(err) {
     console.error(err)
     return NextResponse.json({error: 'test'}, {status: 400})
   }
 
 
+}
+
+export const config = {
+  api: {
+    bodyParser: true
+  }
 }
